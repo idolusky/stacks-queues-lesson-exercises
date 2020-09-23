b172f01d0936f118a9f986c31b2189483cbe68e7
idolusky <idoluski@gmail.com>
2020-09-23 19:43:54 +0300

    exercises 1 and 2 done

--- src/MinStack.js
+++ src/MinStack.js
@@ -1,13 +1,63 @@
 /* Write your class below */
 
+class MinStack {
+    constructor() {
+        this.stack = []
+        this.length = 0
+        this.obj = {}
+    }
 
+    push(x) {
+        this.stack[this.length] = x
+        this.length++
+        this.obj[x] = x
+    }
+    pop() {
+        const last = this.stack[this.length - 1]
+        this.stack.splice((this.length - 1), 1)
+        this.length = this.length > 0 ? this.length - 1 : 0
+        delete this.obj[last]
+        return last
+    }
+    peek() {
+        return this.stack[this.length - 1] || null
+    }
+    isEmpty() {
+        return this.length === 0
+    }
+    print() {
+        console.log(this.stack)
+    }
+    getMin() {
+        if (this.length > 0) {
+            let arr = Object.values(this.obj);
+            let min = Math.min(...arr);
 
+            return min
+        } else {
+            return null
+        }
+    }
 
+}
 
-
-
-
-//You can paste the test code from the lesson below to test your solution
+let ms = new MinStack()
+ms.push(17)
+ms.push(12)
+ms.push(31)
+console.log(ms.getMin())    //12
+ms.pop()
+ms.pop()
+ms.pop()
+console.log(ms.getMin())    //null
+ms.push(19)
+ms.push(21)
+console.log(ms.getMin())    //19
+ms.push(3)
+console.log(ms.getMin())    //3
+ms.pop()
+ms.pop()
+console.log(ms.getMin())    //19
 
 
 
--- src/Queue.js
+++ src/Queue.js
@@ -1,13 +1,53 @@
 /* Write your class below */
 
+class Queue {
+    constructor() {
+        this.queue = []
+        this.length = 0
+    }
+
+    enqueue(x) {
+        this.queue[this.length] = x
+        this.length++
+    }
+
+    isEmpty() {
+        return this.length === 0
+    }
+
+    peek() {
+        if (this.length > 0) {
+            return this.queue[0]
+        } else {
+            return null
+        }
+    }
+
+    dequeue() {
+        const first = this.queue[0]
+        this.queue.splice(0, 1)
+        this.length--
+        return first
+
+    }
+    print() {
+        console.log(this.queue);
+    }
+}
 
 
-
-
-
-
-
-//You can paste the test code from the lesson below to test your solution
+let q = new Queue()
+console.log(q.isEmpty())    //true
+q.print()                   //[]
+q.enqueue(2)
+console.log(q.isEmpty())    //false
+q.enqueue(4)
+q.print()                   //[4,2]
+console.log(q.peek())       //2
+q.dequeue()
+q.dequeue()
+console.log(q.peek())       //null
+console.log(q.isEmpty())    //true
 
 
 
